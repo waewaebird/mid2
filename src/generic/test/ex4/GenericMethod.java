15788b37761cb3cbc25d65338e4bf95fe739db12
jzangeva <jzangeva@auto-focus.co.kr>
2025-05-20 16:54:32 +0900

    ~ 제네릭 - Generic2 - 제네릭 메서드

---
+++ src/generic/test/ex4/GenericMethod.java
@@ -0,0 +1,18 @@
+package generic.test.ex4;
+
+public class GenericMethod { // 여기다가 하면 제네릭 타입
+    public static Object objMethod(Object obj) {
+        System.out.println("Object print : " + obj);
+        return obj;
+    }
+
+    public static<T> T genericMethod(T t) { // 여기는 제네릭 메써드
+        System.out.println("generic print : " + t);
+        return t;
+    }
+
+    public static<T extends Number> T numberMethod(T t) { // 여기는 제네릭 메써드
+        System.out.println("bound print : " + t);
+        return t;
+    }
+}

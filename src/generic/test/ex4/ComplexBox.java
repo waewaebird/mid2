6d98980088b6df2a83fed280d597e96f7fc3eea5
jzangeva <jzangeva@auto-focus.co.kr>
2025-05-23 16:55:35 +0900

    ~ 제네릭 - Generic2 - 제네릭 메서드 활용

---
+++ src/generic/test/ex4/ComplexBox.java
@@ -0,0 +1,20 @@
+package generic.test.ex4;
+
+import generic.animal.Animal;
+
+public class ComplexBox<T extends Animal> { //제네릭 타입
+
+    private T animal;
+
+    public void set(T animal) {
+        this.animal = animal;
+    }
+
+    public <T> T printAndReturn(T t) {
+        System.out.println("animal.className : " + animal.getClass().getName());
+        System.out.println("t.className : " +t.getClass().getName());
+
+        return t;
+    }
+
+}

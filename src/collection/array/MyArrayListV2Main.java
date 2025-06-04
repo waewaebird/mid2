4f99eb955858a7263e2bfd8bcb9a36beb998fc64
waewaebird <jzangeva@gmail.com>
2025-06-04 22:25:09 +0900

    ~ 컬렉션 프레임워크 - ArrayList - 직접 구현하는 배열 리스트3 - 기능 추가

---
+++ src/collection/array/MyArrayListV2Main.java
@@ -0,0 +1,25 @@
+package collection.array;
+
+public class MyArrayListV2Main {
+
+    public static void main(String[] args) {
+        MyArrayListV2 list = new MyArrayListV2(2);
+        System.out.println("==데이터 추가==");
+        System.out.println(list);
+        list.add("a");
+        System.out.println(list);
+        list.add("b");
+        System.out.println(list);
+        list.add("c");
+        System.out.println(list);
+        list.add("d");
+        System.out.println(list);
+        list.add("e");
+        System.out.println(list);
+
+        //범위 초과, capacity가 늘어나지 않으면 예외발생
+        list.add("f");
+        System.out.println(list);
+
+    }
+}

3b4c10f54654815642196929e108e841add14923
jzangeva <jzangeva@auto-focus.co.kr>
2025-06-12 16:53:08 +0900

    ~ 컬렉션 프레임워크 - List - 리스트 추상화2 - 의존관계 주입

---
+++ src/collection/list/MyArrayList.java
@@ -0,0 +1,107 @@
+package collection.list;
+
+import java.util.Arrays;
+
+public class MyArrayList<E> implements MyList<E> {
+    private static final int DEFAULT_CAPACITY = 5;
+
+    private Object[] elementData;
+    private int size = 0;
+
+    public MyArrayList() {
+        elementData = new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyArrayList(int initialCapacity) {
+        elementData = new Object[initialCapacity];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void add(E e) {
+        if(size == elementData.length) {
+            grow();
+        }
+        elementData[size] = e;
+        size++;
+    }
+
+    @Override
+    public void add(int index, E e) {
+        if(size == elementData.length) {
+            grow();
+        }
+        //데이터 이동
+        shiftRightFrom(index);
+        elementData[index] = e;
+        size++;
+    }
+
+    private void shiftRightFrom(int index) {
+        for (int i = size; i > index ; i--) {
+            elementData[i] = elementData[i-1];
+        }
+    }
+
+    //코드 추가
+    public void grow() {
+        int oldCapacity = elementData.length;
+        int newCapacity = oldCapacity * 2;
+
+        //배열을 새로 만들고, 기존 배열을 새로운 배열에 복사
+
+        /*Object[] newArr = new Object[newCapacity];
+
+        for (int i = 0; i < elementData.length; i++) {
+            newArr[i] = elementData[i];
+        }*/
+        elementData = Arrays.copyOf(elementData, newCapacity);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public E get(int index) {
+        return (E) elementData[index];
+    }
+
+    @Override
+    public E set(int index, E element) {
+        E oldValue = get(index);
+        elementData[index] = element;
+        return oldValue;
+    }
+
+
+    @Override
+    public E remove(int index) {
+        E oldValue = get(index);
+        shiftLeftFrom(index);
+        //데이터 이동
+        size--;
+        elementData[size] = null;
+        return oldValue;
+    }
+
+    private void shiftLeftFrom(int index) {
+        for (int i = index; i < size -1; i++) {
+            elementData[i] = elementData[i+1];
+        }
+    }
+
+    public int indexOf(E o) {
+        for (int i = 0; i < size; i++) {
+            if(o.equals(elementData[i])){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public String toString() {
+        return Arrays.toString(Arrays.copyOf(elementData,size)) +
+                " size = " + size + ", capacity" + elementData.length;
+    }
+}

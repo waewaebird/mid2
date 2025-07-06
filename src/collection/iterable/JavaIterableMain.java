08cdd3db9d636cbb741835596f5a0a5fc4f63ea8
jzangeva <jzangeva@auto-focus.co.kr>
2025-07-06 21:02:47 +0900

    ~ 순회3 - 자바가 제공하는 Iterable, Iterator

---
+++ src/collection/iterable/JavaIterableMain.java
@@ -0,0 +1,36 @@
+package collection.iterable;
+
+import java.util.*;
+
+public class JavaIterableMain {
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        printAll(list.iterator());
+        foreach(list);
+
+        Set<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        printAll(set.iterator());
+        foreach(set);
+    }
+
+    private static void printAll(Iterator<Integer> iterator) {
+        System.out.println("iterator = " + iterator.getClass());
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+
+    private static void foreach(Iterable<Integer> iterable) {
+        System.out.println("iterable.getClass() = " + iterable.getClass());
+        for(Integer i : iterable) {
+            System.out.println(i);
+        }
+    }
+}
